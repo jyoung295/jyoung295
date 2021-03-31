1c84719ea868646f1e8b046dab93dde88b4e6581
Jesse Young <young295@purdue.edu>
2021-03-31 11:22:22 -0400

    Update NowPlaying.tsx
    update progress bar to Spotify brand green.

--- components/NowPlaying.tsx
+++ components/NowPlaying.tsx
@@ -59,7 +59,7 @@ export const Player: React.FC<Props> = ({
               width: 100%;
               height: 6px;
               transform-origin: left center;
-              background-color: #24292e;
+              background-color: #1DB954;
               animation: progress ${duration}ms linear;
               animation-delay: -${progress}ms;
             }
